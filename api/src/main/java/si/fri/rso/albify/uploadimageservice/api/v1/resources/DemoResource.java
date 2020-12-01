43480f76a09de898e0442ce6a0d0b32d13e59c49
Anze Mur <am3158@student.uni-lj.si>
2020-12-01 21:52:03 +0100

    Added Travis links, MS localhost links.

--- api/src/main/java/si/fri/rso/albify/uploadimageservice/api/v1/resources/DemoResource.java
+++ api/src/main/java/si/fri/rso/albify/uploadimageservice/api/v1/resources/DemoResource.java
@@ -44,10 +44,10 @@ public class DemoResource {
         DemoData data = new DemoData();
 
         String[] clani = new String[]{"jb6407", "am3158"};
-        String opis = "Aplikacija za upravljanje s slikami.";
-        String[] mikrostoritve = new String[]{"TODO"};
+        String opis = "Aplikacija za upravljanje s slikami. Uporabnik lahko ustvarja nove albume v katere lahko dodaja slike.";
+        String[] mikrostoritve = new String[]{"localhost:8080/v1/albums", "localhost:8081/v1/images"};
         String[] github = new String[]{"https://github.com/RSOTeam13/upload-image-service", "https://github.com/RSOTeam13/albums-service", "https://github.com/RSOTeam13/images-service", "https://github.com/RSOTeam13/users-service"};
-        String[] travis = new String[]{"TODO"};
+        String[] travis = new String[]{"https://travis-ci.com/github/RSOTeam13/upload-image-service", "https://travis-ci.com/github/RSOTeam13/albums-service", "https://travis-ci.com/github/RSOTeam13/images-service", "https://travis-ci.com/github/RSOTeam13/users-service"};
         String[] dockerhub = new String[]{"https://hub.docker.com/r/anzemur/upload-image-service-api", "https://hub.docker.com/r/anzemur/album-service-api", "https://hub.docker.com/r/anzemur/image-service-api", "https://hub.docker.com/r/anzemur/users-service-api"};
 
 
