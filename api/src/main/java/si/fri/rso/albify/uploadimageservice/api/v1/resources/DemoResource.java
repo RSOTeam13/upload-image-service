1205666a75d7809e51876560d5d665a821058ef7
BerciTheBeast <bernard_jaka@hotmail.com>
2020-12-02 14:52:30 +0100

    Image and album service links

--- api/src/main/java/si/fri/rso/albify/uploadimageservice/api/v1/resources/DemoResource.java
+++ api/src/main/java/si/fri/rso/albify/uploadimageservice/api/v1/resources/DemoResource.java
@@ -45,7 +45,7 @@ public class DemoResource {
 
         String[] clani = new String[]{"jb6407", "am3158"};
         String opis = "Aplikacija za upravljanje s slikami. Uporabnik lahko ustvarja nove albume v katere lahko dodaja slike.";
-        String[] mikrostoritve = new String[]{"localhost:8080/v1/albums", "localhost:8081/v1/images"};
+        String[] mikrostoritve = new String[]{"http://20.50.224.183:8080/v1/albums", "http://52.143.9.159:8080/v1/images"};
         String[] github = new String[]{"https://github.com/RSOTeam13/upload-image-service", "https://github.com/RSOTeam13/albums-service", "https://github.com/RSOTeam13/images-service", "https://github.com/RSOTeam13/users-service"};
         String[] travis = new String[]{"https://travis-ci.com/github/RSOTeam13/upload-image-service", "https://travis-ci.com/github/RSOTeam13/albums-service", "https://travis-ci.com/github/RSOTeam13/images-service", "https://travis-ci.com/github/RSOTeam13/users-service"};
         String[] dockerhub = new String[]{"https://hub.docker.com/r/anzemur/upload-image-service-api", "https://hub.docker.com/r/anzemur/album-service-api", "https://hub.docker.com/r/anzemur/image-service-api", "https://hub.docker.com/r/anzemur/users-service-api"};
